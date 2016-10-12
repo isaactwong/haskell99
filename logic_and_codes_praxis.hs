21d69b74fd065ae912ab171a5fc027bba64197c5
isaactwong <isaactwong@gmail.com>
2016-10-12 07:51:25 -0400

    Added truth predicates for problem 46.

---
+++ logic_and_codes_praxis.hs
@@ -0,0 +1,38 @@
+-- Problem 46: Define predicates and/2, or/2, nand/2, nor/2, xor/2, impl/2 and equ/2 (for logical equivalence) which succeed or fail according to the result of their respective operations; e.g. and(A,B) will succeed, if and only if both A and B succeed.
+-- Now, write a predicate table/3 which prints the truth table of a given logical expression in two variables.
+
+not' :: Bool -> Bool
+not' True = False
+not' False = True
+
+and' :: Bool -> Bool -> Bool
+and' True True = True
+and' _ _ = False
+
+or' :: Bool -> Bool -> Bool
+or' False False = False
+or' _ _ = True
+
+
+nand' :: Bool -> Bool -> Bool
+nand' a b = not' $ and' a b
+
+nor' :: Bool -> Bool -> Bool
+nor' a b = not' (or' a b)
+
+xor' :: Bool -> Bool -> Bool
+xor' True False = True
+xor' False True = True
+xor' _ _ = False
+
+-- A implies B, otherwise False.
+impl' :: Bool -> Bool -> Bool
+impl' True True = True
+impl' False False = True
+impl' True False = False
+impl' False True = True
+
+equ' :: Bool -> Bool -> Bool
+equ' True True = True
+equ' False False = True
+equ' _ _ = False
