ce46e9e6126bddf919de1e3481c992c5b2e4d055
isaactwong <isaactwong@gmail.com>
2016-10-17 23:22:02 -0400

    Finished problem 49.

--- logic_and_codes_praxis.hs
+++ logic_and_codes_praxis.hs
@@ -62,3 +62,13 @@ tablen n f = mapM_ putStrLn [toStr a ++ " => " ++ show (f a) | a <- args n]
           toStr = unwords . map (\x -> show x ++ space x)
           space True = "  "
           space False = " "
+
+-- Problem 49: Gray codes. An n-bit Gray code is a sequence of n-bit storage strings according to certain rules.
+-- n = 1: C(1) = ['0','1'].
+-- n = 2: C(2) = ['00','01','11','10'].
+-- n = 3: C(3) = ['000','001','011','010',´110´,´111´,´101´,´100´].
+-- Find out the construction rules and write a predicate with the following specification:
+-- Gray codes are codes where each successive word differs from those around in only one position. So we can't just do replicateM n [0,1].
+gray :: Int -> [String]
+gray 0 = [""]
+gray n = ["0" ++ x | x <- gray (n-1)] ++ ["1" ++ x | x <- (reverse . gray) (n-1)]
