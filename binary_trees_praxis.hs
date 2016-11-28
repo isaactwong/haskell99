7074f9e58806365f9f72f60c549914669656b6c2
isaactwong <isaactwong@gmail.com>
2016-11-27 22:27:09 -0500

    Completed problem 55 with a little help from my friends.

--- binary_trees_praxis.hs
+++ binary_trees_praxis.hs
@@ -3,6 +3,12 @@
 
 -- Problem 55: Construct completely balanced binary trees.
 -- Binary trees are defined to be balanced if |#-nodes-left-subtree - #-nodes-right-subtree| <= 1
-data BinaryTree a = Leaf a | (Branch a (BinaryTree a) (BinaryTree a) 
+data BinaryTree a = Empty | Branch a (BinaryTree a) (BinaryTree a) deriving Show
 
 balancedBinaryTrees :: Int -> [BinaryTree Char]
+balancedBinaryTrees 0 = [Empty]
+balancedBinaryTrees n = let (q, r) = quotRem (n-1) 2
+                    in [Branch 'x' left right | i <- [q..(q+r)],
+                       left <- balancedBinaryTrees i,
+                       right <- balancedBinaryTrees (n-i-1)]
+                       
