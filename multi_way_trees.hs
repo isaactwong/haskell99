d029cd19a56fcacfb70cd43ce2663edb2a4f7119
isaactwong <isaactwong@gmail.com>
2017-01-24 22:59:11 -0500

    Finished multi-way tree problems.

--- multi_way_trees.hs
+++ multi_way_trees.hs
@@ -78,3 +78,21 @@ Write a predicate bottom_up(Tree,Seq) which constructs the bottom-up sequence of
 bottom_up :: (Show a) => (Tree a) -> [a]
 bottom_up (Node x []) = [x]
 bottom_up (Node x ts) = concat (map bottom_up ts) ++ [x]
+
+{-
+Problem 73
+Lisp-like tree representation.
+
+There is a particular notation for multiway trees in Lisp. Lisp is a prominent functional programming language, which is used primarily for artificial intelligence problems. As such it is one of the main competitors of Prolog. In Lisp almost everything is a list, just as in Prolog everything is a term.
+
+The following pictures show how multiway tree structures are represented in Lisp.
+
+Note that in the "lispy" notation a node with successors (children) in the tree is always the first element in a list, followed by its children. The "lispy" representation of a multiway tree is a sequence of atoms and parentheses '(' and ')', which we shall collectively call "tokens". We can represent this sequence of tokens as a Prolog list; e.g. the lispy expression (a (b c)) could be represented as the Prolog list ['(', a, '(', b, c, ')', ')']. Write a predicate tree_ltl(T,LTL) which constructs the "lispy token list" LTL if the tree is given as term T in the usual Prolog notation.
+-}
+
+lisp_tree :: (Tree Char) -> String
+lisp_tree (Node x ts) = "(" ++ [x] ++ (concat (map f ts)) ++ ")"
+          where
+                f (Node y ys)
+                  | length ys == 0 = [y]
+                  | otherwise      = lisp_tree (Node y ys)
